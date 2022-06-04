86147574b73079efe9c37f19dea3da1d498423c4
henriquepsantos <henriquepsantos@gmail.com>
2022-06-04 18:43:38 +0100

    ???

--- pwsh-init.ps1
+++ pwsh-init.ps1
@@ -6,7 +6,8 @@ if (-not $env:XDG_CONFIG_HOME) {
 	$env:XDG_CONFIG_HOME = "$PSScriptRoot\.config"
 	$env:XDG_DATA_HOME = "$PSScriptRoot\.data"
 }
-$env:PATH += ";$env:APPDATA\Python\Python310\Scripts"
+
+if (-not $Loaded) {$env:PATH += ";$env:APPDATA\Python\Python310\Scripts"}
 
 # 	bat
 # 	delta
@@ -27,9 +28,13 @@ Function CD_CONFIG {cd $env:XDG_CONFIG_HOME}
 Set-Alias -Name dots -Value CD_CONFIG
 Set-Alias -Name wg -Value winget
 
+Set-Alias -Name act -Value .\.venv\Scripts\Activate.ps1
+Set-Alias -Name deact -Value deactivate
+
 $env:NVIM_LISTEN_ADDRESS = "\\.\pipe\nvim-nvr"
 function nvr { nvr.exe -s --nostart $args; if ($LastExitCode -ne 0) { nvd; if ($args) { nvr.exe -s --nostart $args } } }
 function nvd { neovide --multigrid -- --listen "$env:NVIM_LISTEN_ADDRESS" }
 
 oh-my-posh init pwsh --config 'C:\Program Files (x86)\oh-my-posh\themes\powerlevel10k_classic.omp.json' | Invoke-Expression
 
+$Loaded = 1
