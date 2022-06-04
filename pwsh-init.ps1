79cb1a169c30ad51e434c15d276695fb91abd04c
henriquepsantos <henriquepsantos@gmail.com>
2022-06-04 01:38:20 +0100

    Added nvr and neovide aliases for remote editing

--- pwsh-init.ps1
+++ pwsh-init.ps1
@@ -6,9 +6,30 @@ if (-not $env:XDG_CONFIG_HOME) {
 	$env:XDG_CONFIG_HOME = "$PSScriptRoot\.config"
 	$env:XDG_DATA_HOME = "$PSScriptRoot\.data"
 }
+$env:PATH += ";$env:APPDATA\Python\Python310\Scripts"
 
-Set-Alias -Name nvd -Value neovide
+# 	bat
+# 	delta
+# 	fd
+# 	fzf
+# 	grex
+# 	hyperfine
+# 	just
+# 	ninja
+# 	rg
+# 	ruplacer
+# 	sad
+# 	tokei
+# 	typos
+
+Function CD_CONFIG {cd $env:XDG_CONFIG_HOME}
+
+Set-Alias -Name dots -Value CD_CONFIG
+Set-Alias -Name wg -Value winget
+
+$env:NVIM_LISTEN_ADDRESS = "\\.\pipe\nvim-nvr"
+function nvr { nvr.exe -s --nostart $args; if ($LastExitCode -ne 0) { nvd; if ($args) { nvr.exe -s --nostart $args } } }
+function nvd { neovide -- --listen "$env:NVIM_LISTEN_ADDRESS" }
 
 oh-my-posh init pwsh --config 'C:\Program Files (x86)\oh-my-posh\themes\powerlevel10k_classic.omp.json' | Invoke-Expression
-cls
 
