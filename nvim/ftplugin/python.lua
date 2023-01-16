a8f66482d3e350c31dfa1a7bd4aee52bece392ed
dartacao <henriquepsantos@gmail.com>
2023-01-16 16:21:46 +0000

    changes to improve performance, fuck windows and its spaces

--- nvim/ftplugin/python.lua
+++ nvim/ftplugin/python.lua
@@ -1,7 +1,7 @@
 -- Run file in terminal
-vim.keymap.set('n', 'çç', ':TermExec cmd="python %"<CR>', {silent = true, buffer = true})
+vim.keymap.set('n', 'çç', function() require('toggleterm').exec_command([[cmd="python ]]..vim.fn.expand('%:.')..[["]]) end, {silent = true, buffer = true})
 
 --> NOTE: Buffer local commands for opening Jupyter and using isort to sort imports
-vim.cmd('command! -buffer JPC :vs | terminal jupyter-console.exe<CR>') 
+-- vim.cmd('command! -buffer JPC :vs | terminal jupyter-console.exe<CR>') 
 vim.cmd('command! -buffer Isort :lua require("prog").run_job("isort " .. vim.fn.expand("%"))<CR>') 
 
