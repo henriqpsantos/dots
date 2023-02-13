e8ad3a219f059e17e8fb38e5e2894086c74d77c4
dartacao <henriquepsantos@gmail.com>
2023-02-13 11:01:39 +0000

    NVR is now native

--- pwsh-init.ps1
+++ pwsh-init.ps1
@@ -6,25 +6,11 @@ if (-not $env:XDG_CONFIG_HOME) {
 	$env:XDG_DATA_HOME = "$PSScriptRoot/../.data"
 }
 
-if (-not $Loaded) {$env:PATH += ";$env:APPDATA\Python\Python310\Scripts"}
+# if (-not $Loaded) {$env:PATH += ";C:\Program Files\WPy64-38123\pypy3.8-v7.3.9-win64\Scripts"}
 
 $DOCS = [Environment]::GetFolderPath("MyDocuments")
 $env:XDG_CACHE_HOME = "$DOCS/.cache"
 $env:XDG_DATA_HOME = "$DOCS/.data"
-
-# 	bat
-# 	delta
-# 	fd
-# 	fzf
-# 	grex
-# 	hyperfine
-# 	just
-# 	ninja
-# 	rg
-# 	ruplacer
-# 	sad
-# 	tokei
-# 	typos
 
 Function CD_CONFIG {cd $env:XDG_CONFIG_HOME}
 
@@ -34,10 +20,23 @@ Set-Alias -Name wg -Value winget
 Set-Alias -Name act -Value .\.venv\Scripts\Activate.ps1
 Set-Alias -Name deact -Value deactivate
 
+Set-Alias -Name cd -Value pushd -Option AllScope -Force
+Set-Alias -Name bd -Value popd -Option AllScope
+
+Set-Alias -Name cdl -Option AllScope -Value cdl_
+
+function cdl_ { Get-Location -stack | foreach {$_.Path} | select -uniq | fzf | cd }
+
+# function nvr { nvr.exe -s --nostart -l $args; if ($LastExitCode -ne 0) { nvd; if ($args) { nvr.exe -s --nostart -l $args } } }
+
+# Run neovide with remote mode as option
 $env:NVIM_LISTEN_ADDRESS = "\\.\pipe\nvim-nvr"
-function nvd { neovide --multigrid -- --listen "$env:NVIM_LISTEN_ADDRESS" }
-function nvr { nvr.exe -s --nostart -l $args; if ($LastExitCode -ne 0) { nvd; if ($args) { nvr.exe -s --nostart -l $args } } }
+function nvr {if (-not (Test-Path $env:NVIM_LISTEN_ADDRESS))
+	{ neovide --multigrid --notabs $args.foreach{"$pwd\$_"} -- --listen "$env:NVIM_LISTEN_ADDRESS" }
+	else
+	{ if ($args) {nvim --server "$env:NVIM_LISTEN_ADDRESS" --remote $args.foreach{"$pwd\$_"}} } }
 
 oh-my-posh init pwsh --config 'C:\Program Files (x86)\oh-my-posh\themes\powerlevel10k_classic.omp.json' | Invoke-Expression
 
 $Loaded = 1
+
