16311fb51323919c92fd0f1ab3d6232b05da2920
henriquepsantos <henriquepsantos@gmail.com>
2023-02-14 13:06:50 +0000

    statusline testing

--- nvim/lua/statusline.lua
+++ nvim/lua/statusline.lua
@@ -1,7 +1,7 @@
 local M = {}
 local api = vim.api
 local fn = vim.fn
-M.modes = setmetatable({
+local modes = setmetatable({
 		['n']  = {'Normal', 'N'};
 		['no'] = {'N·Pending', 'N·P'} ;
 		['v']  = {'Visual', 'V' };
@@ -28,14 +28,73 @@ M.modes = setmetatable({
 	end
 })
 
-M.get_current_mode = function()
+
+-- lualine_a = {'mode'},
+-- lualine_b = {},
+-- lualine_c = {'branch', 'diff'},
+-- lualine_y = {'filetype', 'encoding', 'fileformat'},
+-- lualine_z = {require('prog').get_battery_indicator},
+
+-- local function getColorSpec(hlGroup, kind)
+-- 	for _,hlName in ipairs(hlGroup) do
+-- 		if vim.fn.hlexists(hlName) ~= 0 then
+-- 			if kind == 'bg' then
+-- 				return string.format('#%06x', vim.api.nvim_get_hl_by_name(hlName, true).background)
+-- 			else
+-- 				return string.format('#%06x', vim.api.nvim_get_hl_by_name(hlName, true).foreground)
+-- 			end
+-- 		end
+-- 	end
+-- end
+
+-- local colors = {
+-- 	normal  = getColorSpec( { 'PmenuSel', 'PmenuThumb', 'TabLineSel' },'bg'),
+-- 	insert  = getColorSpec( { 'String', 'MoreMsg' },'fg'),
+-- 	replace = getColorSpec( { 'Number', 'Type' },'fg'),
+-- 	visual  = getColorSpec( { 'Special', 'Boolean', 'Constant' },'fg'),
+-- 	command = getColorSpec( { 'Identifier' },'fg'),
+-- 	back1   = getColorSpec( { 'Normal', 'StatusLineNC' },'bg'),
+-- 	fore    = getColorSpec( { 'Normal', 'StatusLine' },'fg'),
+-- 	back2   = getColorSpec( { 'StatusLine' },'bg'),
+-- }
+
+local colors = {
+	active        = '%#StatusLine#',
+	inactive      = '%#StatuslineNC#',
+	mode          = '%#Mode#',
+	mode_alt      = '%#ModeAlt#',
+	git           = '%#Git#',
+	git_alt       = '%#GitAlt#',
+	filetype      = '%#Filetype#',
+	filetype_alt  = '%#FiletypeAlt#',
+	line_col      = '%#LineCol#',
+	line_col_alt  = '%#LineColAlt#',
+}
+
+local sep = ''
+
+local function getCurrentMode()
 	local current_mode = api.nvim_get_mode().mode
-	if self:is_truncated(self.trunc_width.mode) then
-		return string.format(' %s ', modes[current_mode][2]):upper()
-	end
 	return string.format(' %s ', modes[current_mode][1]):upper()
 end
 
-statusline = ""
+local function getFiletype()
+	local file_name, file_ext = fn.expand("%:t"), fn.expand("%:e")
+	local icon = require'nvim-web-devicons'.get_icon(file_name, file_ext, { default = true })
+	local filetype = vim.bo.filetype
 
-return M
+	if filetype == '' then return '' end
+	return string.format(' %s %s ', icon, filetype):lower()
+end
+function getStatusLine()
+	return table.concat({
+		colors.active,
+		colors.filetype,
+		getCurrentMode(),
+		colors.mode_alt,
+		sep,
+		" %<%f ",
+	})
+end
+
+vim.o.statusline = "%!luaeval('getStatusLine()')"
