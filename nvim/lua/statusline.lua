ebb6f4d2c1f3d49a822a6ab457353c12bd1f377f
henriquepsantos <henriquepsantos@gmail.com>
2023-02-15 00:14:09 +0000

    statusline: done?

--- nvim/lua/statusline.lua
+++ nvim/lua/statusline.lua
@@ -1,5 +1,6 @@
 local api = vim.api
 local fn = vim.fn
+
 local modes = setmetatable({
 		['n']  = {'Normal', 'N'};
 		['no'] = {'N·Pending', 'N·P'} ;
@@ -14,94 +15,107 @@ local modes = setmetatable({
 		['R']  = {'Replace', 'R'};
 		['Rv'] = {'V·Replace', 'V·R'};
 		['c']  = {'Command', 'C'};
-		['cv'] = {'Vim·Ex ', 'V·E'};
-		['ce'] = {'Ex ', 'E'};
-		['r']  = {'Prompt ', 'P'};
-		['rm'] = {'More ', 'M'};
-		['r?'] = {'Confirm ', 'C'};
-		['!']  = {'Shell ', 'S'};
-		['t']  = {'Terminal ', 'T'};
+		['cv'] = {'Vim·Ex', 'V·E'};
+		['ce'] = {'Ex', 'E'};
+		['r']  = {'Prompt', 'P'};
+		['rm'] = {'More', 'M'};
+		['r?'] = {'Confirm', 'C'};
+		['!']  = {'Shell', 'S'};
+		['t']  = {'Terminal', 'T'};
 	}, {
 	__index = function()
 		return {'Unknown', 'U'} -- handle edge cases
 	end
 })
 
+local function getSpecs(name)
+	print(name)
+	for k,v in pairs(vim.api.nvim_get_hl_by_name(name, true)) do
+		if type(v) == 'number' then
+			print(string.format('  %s, #%06x', k, v))
+		else
+			print(string.format('  %s, %s', k, v))
+		end
+	end
+end
 
--- set_hl(0, 'SLback1', {bg = '#292522'})
--- set_hl(0, 'SLback2', {bg = '#34302c'})
---
--- set_hl(0, 'SLnmode', {bg = '#ece1d7', fg = '#292522', bold = true})
--- set_hl(0, 'SLimode', {bg = '#a3a9ce', fg = '#292522', bold = true})
--- set_hl(0, 'SLrmode', {bg = '#cf9bc2', fg = '#292522', bold = true})
--- set_hl(0, 'SLvmode', {bg = '#b380b0', fg = '#292522', bold = true})
--- set_hl(0, 'SLterm',  {bg = '#ebc06d', fg = '#292522', bold = true})
---
--- set_hl(0, '_inv_SLnmode', {fg = '#ece1d7', bg = '#292522', bold = true})
--- set_hl(0, '_inv_SLimode', {fg = '#a3a9ce', bg = '#292522', bold = true})
--- set_hl(0, '_inv_SLrmode', {fg = '#cf9bc2', bg = '#292522', bold = true})
--- set_hl(0, '_inv_SLvmode', {fg = '#b380b0', bg = '#292522', bold = true})
--- set_hl(0, '_inv_SLterm',  {fg = '#ebc06d', bg = '#292522', bold = true})
---
--- set_hl(0, 'SLcmode', {bg = '#7d2a2f', fg = '#ece1d7', bold = true})
--- set_hl(0, '_inv_SLcmode', {fg = '#7d2a2f', bg = '#292522', bold = true})
---
--- set_hl(0, 'SLfore',  {fg = '#ece1d7'})
+local function setHighlight(name, attrs, invert)
+	vim.api.nvim_set_hl(0, name, attrs)
+	if invert then
+		local temp = attrs.bg
+		attrs.bg = attrs.fg
+		attrs.fg = temp
+		vim.api.nvim_set_hl(0, '_inv_'..name, attrs)
+	end
+end
 
-local clmodes = setmetatable({
-		['n']  = "%#SpecialChar#";
-		['no'] = "%#SpecialChar#";
-		['v']  = "%#DiffChange#";
-		['V']  = "%#DiffChange#";
-		[''] = "%#DiffChange#";
-		['s']  = "%#Operator#";
-		['S']  = "%#Operator#";
-		[''] = "%#Operator#";
-		['i']  = "%#Constant#";
-		['ic'] = "%#Constant#";
-		['R']  = "%#Character#";
-		['Rv'] = "%#Character#";
-		['c']  = "%#ErrorMsg#";
-		['cv'] = "%#ErrorMsg#";
-		['ce'] = "%#ErrorMsg#";
-		['r']  = "%#StatusLine#";
-		['rm'] = "%#StatusLine#";
-		['r?'] = "%#StatusLine#";
-		['!']  = "%#Function#";
-		['t']  = "%#Function#";
+local SLInvHighlights = {
+	['SLnmode'] = {bg = '#ece1d7', fg = '#292522', bold = true},
+	['SLimode'] = {bg = '#a3a9ce', fg = '#292522', bold = true},
+	['SLrmode'] = {bg = '#cf9bc2', fg = '#292522', bold = true},
+	['SLvmode'] = {bg = '#b380b0', fg = '#292522', bold = true},
+	['SLterm']  = {bg = '#ebc06d', fg = '#292522', bold = true},
+}
+local SLHighlights = {
+	['SLback1'] = {bg = '#292522'},
+	['SLback2'] = {bg = '#34302c'},
+	['SLfore'] = {fg = '#ece1d7'},
+	['SLcmode'] = {bg = '#7d2a2f', fg = '#ece1d7', bold = true},
+	['_inv_SLcmode'] = {fg = '#7d2a2f', bg = '#292522', bold = true},
+}
+
+for i,v in pairs(SLInvHighlights) do setHighlight(i, v, true) end
+for i,v in pairs(SLHighlights) do setHighlight(i, v, false) end
+
+local mode_colors = setmetatable({
+		['n']  = "SLnmode";
+		['no'] = "SLnmode";
+		['v']  = "SLvmode";
+		['V']  = "SLvmode";
+		[''] = "SLvmode";
+		['s']  = "SLrmode";
+		['S']  = "SLrmode";
+		[''] = "SLrmode";
+		['i']  = "SLimode";
+		['ic'] = "SLimode";
+		['R']  = "SLrmode";
+		['Rv'] = "SLrmode";
+		['c']  = "SLcmode";
+		['cv'] = "SLcmode";
+		['ce'] = "SLcmode";
+		['r']  = "StatusLine";
+		['rm'] = "StatusLine";
+		['r?'] = "StatusLine";
+		['!']  = "SLterm";
+		['t']  = "SLterm";
 	}, {
 	__index = function()
-		return "%#StatusLine#" -- handle edge cases
+		return "StatusLine" -- handle edge cases
 	end
 })
 
-local function getCurrentMode()
-	local current_mode = api.nvim_get_mode().mode
-	return string.format('%s %s ', clmodes[current_mode], modes[current_mode][1]):upper()
+local EQUAL = '%='
+local SEP = {'', '', '', ''}
+local SEPARATOR = "|"
+
+local function wrap_hl(hl)
+	return table.concat({"%#", hl, "#"})
 end
 
-local function getFiletype()
-	local file_name, file_ext = fn.expand("%:t"), fn.expand("%:e")
-	local icon = require'nvim-web-devicons'.get_icon(file_name, file_ext, { default = true })
-	local filetype = vim.bo.filetype
-
-	if filetype == '' then return '' end
-	return string.format(' %s %s ', icon, filetype):lower()
+local function getCurrentMode()
+	local current_mode = api.nvim_get_mode().mode
+	return string.format('%s %s %s%s', wrap_hl(mode_colors[current_mode]), modes[current_mode][1]:upper(), wrap_hl("_inv_"..mode_colors[current_mode]), SEP[3])
 end
 
 local function getGitStatus()
 	-- use fallback because it doesn't set this variable on the initial `BufEnter`
 	local signs = vim.b.gitsigns_status_dict or {head = '', added = 0, changed = 0, removed = 0}
 	local is_head_empty = signs.head ~= ''
-
-	return is_head_empty
-		and string.format(' %%#GitSignsAdd#+%s %%#GitSignsChange#~%s %%#GitSignsDelete#-%s %%#Normal#|  %s ',
-		signs.added, signs.changed, signs.removed, signs.head) or ''
+	return is_head_empty and string.format(' %s+%s %s~%s %s-%s %%#Normal#|  %s %s',
+			wrap_hl('GitSignsAdd'), signs.added,
+			wrap_hl('GitSignsChange'), signs.changed,
+			wrap_hl('GitSignsDelete'), signs.removed, signs.head, SEP[1]) or ''
 end
-
-local cls = {
-	active		= '%#Normal#',
-}
 
 local function getFiletype()
 	local file_name, file_ext = fn.expand("%:t"), fn.expand("%:e")
@@ -118,25 +132,22 @@ local function getFileInfo()
 	return string.format(' %s | %s ', icon_ff, icon_enc)
 end
 
-local EQUAL = '%='
-local SEPARATOR = cls.active..'|'
+local norm = wrap_hl('Normal')
+local center = wrap_hl('SLnmode')
+local center_inv = wrap_hl('_inv_SLnmode')
 
-return function()
+function x()
 	return table.concat({
-		cls.active,
 		getCurrentMode(),
-		SEPARATOR,
 		getGitStatus(),
 		EQUAL,
-		" %<%t ",
-		SEPARATOR,
-		getFiletype(),
-		SEPARATOR,
-		getFileInfo(),
-		EQUAL,
-		" Ln %l /  %L ",
-		SEPARATOR,
+		center_inv, SEP[4], center, " %<%t ", SEPARATOR, getFiletype(), SEPARATOR, getFileInfo(),
+		norm, SEP[3], EQUAL, SEP[2], " Ln %l /  %L ",
+		SEP[2],
 		" ",
 		require('prog').get_battery_indicator(),
 	})
 end
+
+vim.o.statusline = [[%!luaeval('x()')]]
+
